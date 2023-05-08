44862a91632be42c5faa1fc51f547dffae5e90a8
delong34 <delong@bk.ru>
2023-05-08 18:08:30 +0300

    Сделал 9 ДЗ

---
+++ lesson9/lesson9/filecontroller.h
@@ -0,0 +1,28 @@
+#ifndef FILECONTROLLER_H
+#define FILECONTROLLER_H
+
+#include <QObject>
+#include <QFile>
+
+class FileController : public QObject
+{
+    Q_OBJECT
+public:
+    explicit FileController(QObject *parent = nullptr);
+    virtual ~FileController();
+    Q_INVOKABLE
+    void getNextTask();
+    Q_INVOKABLE
+    void saveData(QString taskName, QString deadline, int progress);
+    void writeDataVec(QString taskName, QString deadline, int progress);
+
+signals:
+    void loadTask(QString taskName, QString deadline, int progress);
+
+private:
+    QFile* file_;
+    QVector<QString> taskNames_;
+    QVector<QString> deadlines_;
+    QVector<int> progresses_;
+};
+#endif // FILECONTROLLER_H
